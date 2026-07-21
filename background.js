041dbde9abfbc73368d461a85cc5835f41a7f93a
thulanesigasa <pharezsigasa@gmail.com>
2026-07-21 11:25:44 +0200

    Initial structure

---
+++ background.js
@@ -0,0 +1,2 @@
+// Background Service Worker
+// Secure API calls will be implemented here in Phase 4
---
+++ content.js
@@ -0,0 +1,2 @@
+// Content Script
+// DOM reading and UI injection will be implemented here in Phases 2, 3, and 5
